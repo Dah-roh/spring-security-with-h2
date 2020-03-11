fe07e9c5a0f511d07086ccb511d55f687285ade7
dizsart <darogadibia@gmail.com>
2020-03-11 07:41:13 +0100

    added the webConfig file to the file structure of the application

--- src/main/java/com/security/userdetailsservice/Configuration/WebConfig.java
+++ src/main/java/com/security/userdetailsservice/Configuration/WebConfig.java
@@ -3,6 +3,7 @@ package com.security.userdetailsservice.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,22 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/WERB-INF/jsp/*").permitAll()
+                .antMatchers("/resources/**", "/WEB-INF/jsp/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/welcome")
+                .failureUrl("/login?error")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(bycryptPasswordEncoder());
     }
 }
